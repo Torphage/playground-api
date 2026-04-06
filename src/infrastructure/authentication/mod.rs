8a823fc8c6297b4e09bc38f9904face4ef90fabd
Torphage <filip.torphage1@gmail.com>
2026-04-06 18:18:21 +0200

    Implement DefaultRefreshTokenService for fresh token generation

--- src/infrastructure/authentication/mod.rs
+++ src/infrastructure/authentication/mod.rs
@@ -1,5 +1,6 @@
-mod composite_request_authenticator;
+pub mod composite_request_authenticator;
 pub mod jwt;
+pub mod refresh_token_service;
 pub mod session;
 
 pub use composite_request_authenticator::CompositeRequestAuthenticator;
